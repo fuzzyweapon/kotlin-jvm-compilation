a7217411b52b099b4ddf66e77b3ab5b4bc70c5fd
julia <julia@polyforest.com>
2019-05-24 14:47:32 -0700

    Add simplified main for testing

--- consumer/src/jvmMain/kotlin/sample/SampleJvm.kt
+++ consumer/src/jvmMain/kotlin/sample/SampleJvm.kt
@@ -1,14 +1,5 @@
 package sample
 
-import io.ktor.application.*
-import io.ktor.html.*
-import io.ktor.http.content.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import kotlinx.html.*
-import java.io.*
-
 actual class Sample {
     actual fun checkMe() = 42
 }
@@ -18,44 +9,8 @@ actual object Platform {
 }
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-        val currentDir = File(".").absoluteFile
-        environment.log.info("Current directory: $currentDir")
-
-        val webDir = listOf(
-            "web",
-            "../src/jsMain/web",
-            "src/jsMain/web"
-        ).map {
-            File(currentDir, it)
-        }.firstOrNull { it.isDirectory }?.absoluteFile ?: error("Can't find 'web' folder for this sample")
-
-        environment.log.info("Web directory: $webDir")
-
-        routing {
-            get("/") {
-                call.respondHtml {
-                    head {
-                        title("Hello from Ktor!")
-                    }
-                    body {
-                        +"${hello()} from Ktor. Check me value: ${Sample().checkMe()}"
-                        div {
-                            id = "js-response"
-                            +"Loading..."
-                        }
-                        script(src = "/static/require.min.js") {
-                        }
-                        script {
-                            +"require.config({baseUrl: '/static'});\n"
-                            +"require(['/static/bug.js'], function(js) { js.sample.helloWorld('Hi'); });\n"
-                        }
-                    }
-                }
-            }
-            static("/static") {
-                files(webDir)
-            }
-        }
-    }.start(wait = true)
+    val common = CommonClass()
+    val existingParam = "existingParamJVMValue"
+    common.publicMethod(existingParam)
+    common.publicPrivateMethodCaller(existingParam)
 }
